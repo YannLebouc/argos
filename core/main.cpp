b5ce10a1649f678b488f1c039d3ea210364dea8e
YannLebouc <yanlpi@proton.me>
2026-05-22 14:54:19 +0200

    feat: simple manual socket initialisation, binding, listening to connexion and acceptance

--- core/main.cpp
+++ core/main.cpp
@@ -1,4 +1,3 @@
-#include <cstddef>
 #include <cstring>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -7,39 +6,45 @@
 
 int main()
 {
+  constexpr int PORT = 8080;
+  constexpr int BACKLOG = 5;
+  // First socket implementation will be manual and minimalistic for speed and educational purpose
+  // Second version will use the modern posix API with getaddrinfo() etc... 
+
   // Be sure to understand and write down what a "file descriptor" is
-   int socketfd = socket(AF_INET, SOCK_STREAM, 0);  
-   if (socketfd == -1)
-   {
-     // Define error handling stategy, errors vs exceptions
-     // Program should exit on failure here
-   }
-
-   
-  int status;
-  struct addrinfo hints;
-  struct addrinfo *servinfo;
-  std::memset(&hints, 0, sizeof hints);
-
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
-
-  status = getaddrinfo(NULL, "8080", &hints, &servinfo );
-  if (status != 0)
+  int socket_fd = socket(AF_INET, SOCK_STREAM, 0);  
+  if (socket_fd == -1)
   {
-    // Error handling to be defined
-    // Program should exit on failure
+    // Define error handling stategy, errors vs exceptions
+    // Program should exit on failure here
+  }
+
+  sockaddr_in server_socket_addr {};
+  server_socket_addr.sin_family = AF_INET;
+  server_socket_addr.sin_port = htons(PORT);
+  server_socket_addr.sin_addr.s_addr = INADDR_ANY;
+
+  int bound = bind(socket_fd, (struct sockaddr*)&server_socket_addr, sizeof(server_socket_addr));
+  if (bound != 0) 
+  {
+    // Handle error, shutdown program correctly
   }
 
 
-  // bind()
+  int listening = listen(socket_fd, BACKLOG);
+  if (listening != 0)
+  {
+    // Handle error, shutdown program correctly
+  }
 
-  // send()
-  // recv()
-   
+  sockaddr_storage client_addr_info {};
+  socklen_t client_addr_info_size = sizeof(client_addr_info);
 
-  // Do everything needed with servinfo, then free it
-  freeaddrinfo(servinfo);
-
+  int new_socket_fd  = accept(socket_fd, (struct sockaddr*)&client_addr_info, &client_addr_info_size);
+  if (new_socket_fd == -1)
+  {
+    // Handle error, shutdown program correctly
+  }
+  // NEXT SESSION : Receive data from client (recv()), parse it according to to-be-defined procotol, print the result. 
+    
 }
