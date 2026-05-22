0df34d2f66694651bfb63fc40aa7f54a4c66cd9f
YannLebouc <yanlpi@proton.me>
2026-05-22 10:21:53 +0200

    feat: socket informations prep using modern getaddrinfo function instead of manual init

--- core/main.cpp
+++ core/main.cpp
@@ -1,5 +1,9 @@
+#include <cstddef>
+#include <cstring>
 #include <sys/socket.h>
-
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
 
 int main()
 {
@@ -8,6 +12,34 @@ int main()
    if (socketfd == -1)
    {
      // Define error handling stategy, errors vs exceptions
+     // Program should exit on failure here
    }
+
+   
+  int status;
+  struct addrinfo hints;
+  struct addrinfo *servinfo;
+  std::memset(&hints, 0, sizeof hints);
+
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  status = getaddrinfo(NULL, "8080", &hints, &servinfo );
+  if (status != 0)
+  {
+    // Error handling to be defined
+    // Program should exit on failure
+  }
+
+
+  // bind()
+
+  // send()
+  // recv()
+   
+
+  // Do everything needed with servinfo, then free it
+  freeaddrinfo(servinfo);
 
 }
