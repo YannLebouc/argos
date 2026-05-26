41753fbf2a5d113c711379e738764e594c0a0a3f
YannLebouc <yanlpi@proton.me>
2026-05-26 09:57:55 +0200

    feat: handling partial incoming messages by looping on recv() and finding new line separator

--- core/main.cpp
+++ core/main.cpp
@@ -26,16 +26,14 @@ int main()
         return EXIT_FAILURE;
         // Define error handling stategy, errors vs exceptions
     }
-    std::cout << "Main listening socket created, file descriptor number : "
-              << socket_fd << "\n";
+    std::cout << "Main listening socket created, file descriptor number : " << socket_fd << "\n";
 
     sockaddr_in server_socket_addr{};
     server_socket_addr.sin_family = AF_INET;
     server_socket_addr.sin_port = htons(PORT);
     server_socket_addr.sin_addr.s_addr = INADDR_ANY;
 
-    int bound = bind(socket_fd, (struct sockaddr *)&server_socket_addr,
-                     sizeof(server_socket_addr));
+    int bound = bind(socket_fd, (struct sockaddr *)&server_socket_addr, sizeof(server_socket_addr));
 
     if (bound != 0) {
         std::cerr << "Failed to bind socket to port " << PORT << "\n";
@@ -58,14 +56,12 @@ int main()
 
     while (true) {
         int new_socket_fd =
-            accept(socket_fd, (struct sockaddr *)&client_addr_info,
-                   &client_addr_info_size);
+            accept(socket_fd, (struct sockaddr *)&client_addr_info, &client_addr_info_size);
         if (new_socket_fd == -1) {
             std::cerr << "Accept failed\n";
             continue;
         }
-        std::cout << "\n[Client Connected] File descriptor : " << new_socket_fd
-                  << "\n";
+        std::cout << "\n[Client Connected] File descriptor : " << new_socket_fd << "\n";
 
         std::string stream_buffer;
 
@@ -86,22 +82,18 @@ int main()
 
             stream_buffer.append(buffer, bytes_received);
 
-            auto newline_pos = stream_buffer.find('\n');
-            if (newline_pos == std::string::npos) {
-                continue;
+            size_t newline_pos;
+            while ((newline_pos = stream_buffer.find('\n')) != std::string::npos) {
+
+                std::string message = stream_buffer.substr(0, newline_pos);
+                stream_buffer.erase(0, newline_pos + 1);
+
+                std::cout << "Complete message received and treated : " << message << std::endl;
+
+                const std::string validation_msg = "Last message treated :" + message + "\n";
+
+                send(new_socket_fd, validation_msg.c_str(), validation_msg.length(), 0);
             }
-
-            std::string message = stream_buffer.substr(0, newline_pos);
-            stream_buffer.erase(0, newline_pos + 1);
-
-            std::cout << "Message received : " << message << std::endl;
-
-            const std::string validation_msg =
-                "Last message treated :" + message + "\n";
-            send(new_socket_fd, validation_msg.c_str(), validation_msg.length(),
-                 0);
-
-            break;
         }
         close(new_socket_fd);
         std::cout << "[Client disconnected] socket closed";
